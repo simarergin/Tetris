0a40e0e3c0438900cbb3cbf4900773311c09b7a3
simarergin <simarergin@hotmail.com>
2023-03-16 16:29:14 +0300

    Tetris

---
+++ Tetris/Tetris/BlockQueue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Tetris
+{
+    public class BlockQueue
+    {
+        private readonly Block[] blocks = new Block[]
+        {
+            new IBlock(),
+            new JBlocks(),
+            new LBlocks(),
+            new OBlock(),
+            new SBloks(),
+            new TBloks(),
+            new ZBlock()
+        };
+
+        private readonly Random random = new Random();
+
+        public Block NextBlock { get; private set; }
+
+        public BlockQueue()
+        {
+            NextBlock = RandomBlock();
+        }
+
+        private Block RandomBlock()
+        {
+            return blocks[random.Next(blocks.Length)];
+        }
+
+        public Block GetAndUpdate()
+        {
+            Block block = NextBlock;
+
+            do
+            {
+                NextBlock = RandomBlock();
+            }
+            while (block.Id == NextBlock.Id);
+
+            return block;
+        }
+    }
+}
